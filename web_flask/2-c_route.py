2a415a5ec55418dbbb9b14ac80ac8c4e2cb4c476
Emmanuel Adebiyi <adebiyiemmanuel11@gmail.com>
2023-02-06 03:27:21 -0800

    task 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Starts a Flask web application.
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /: Displays 'Hello HBNB!'
+"""
+
+from flask import Flask, escape
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    text = text.replace("_", " ")
+    return "C " + escape(text)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
